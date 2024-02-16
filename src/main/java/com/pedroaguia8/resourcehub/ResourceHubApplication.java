62d91de400096aca53df769f645b3b8211b80eba
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-02-16 01:00:44 +0000

    Generate the project with spring initializr

---
+++ src/main/java/com/pedroaguia8/resourcehub/ResourceHubApplication.java
@@ -0,0 +1,13 @@
+package com.pedroaguia8.resourcehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceHubApplication.class, args);
+	}
+
+}
