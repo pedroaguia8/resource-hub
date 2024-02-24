84a019e8d8016879e4e64d5338af2eb22ca6ecdf
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-02-24 02:34:31 +0000

    Add login and registration + email verification
    Followed this tutorial by Amigoscode:
    https://www.youtube.com/watch?v=QwQuro7ekvc
    https://github.com/amigoscode/login-registration-backend

--- src/main/java/com/pedroaguia8/resourcehub/appuser/AppUser.java
+++ src/main/java/com/pedroaguia8/resourcehub/appuser/AppUser.java
@@ -1,20 +1,20 @@
 package com.pedroaguia8.resourcehub.appuser;
 
-import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor
 @Entity
 public class AppUser implements UserDetails {
@@ -30,29 +30,25 @@ public class AppUser implements UserDetails {
             generator = "appuser_sequence"
     )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public AppUser(String name,
-                   String username,
+    public AppUser(String firstName,
+                   String lastName,
                    String email,
                    String password,
-                   AppUserRole appUserRole,
-                   Boolean locked,
-                   Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                   AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
@@ -68,7 +64,7 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return username;
+        return email;
     }
 
     // Not using this feature
@@ -91,5 +87,21 @@ public class AppUser implements UserDetails {
     @Override
     public boolean isEnabled() {
         return enabled;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        AppUser appUser = (AppUser) o;
+        return getId() != null && Objects.equals(getId(), appUser.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
---
+++ src/main/java/com/pedroaguia8/resourcehub/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.pedroaguia8.resourcehub.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
